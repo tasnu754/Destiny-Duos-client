222d209a9411128218380c181660fa65a217db30
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-11-28 13:30:59 +0600

    user contact page created and request contact deletion implement

--- src/APIs/userDashboeard.js
+++ src/APIs/userDashboeard.js
@@ -12,9 +12,20 @@ export const deleteUserFavBiodata = async (id) => {
     return data;
 }
 
+// delete user contact req biodata item
+export const deleteUserContactReq= async (id) => {
+    const { data } = await axiosSecure.delete(`/deleteUserContactReq/${id}`)
+    return data;
+}
+
 //add success story to success strory collection
 export const addSuccessStroy = async(successStory) => {
     const { data } = await axiosSecure.put('/addSuccessStory' , successStory)
     return data;
 }
 
+// get user contact requests
+export const getUserContactRequest = async (email) => {
+    const { data } = await axiosSecure(`/getUserContactRequests/${email}`)
+    return data;
+}
--- src/Pages/Dashboard/ContactRequests.jsx
+++ src/Pages/Dashboard/ContactRequests.jsx
@@ -1,9 +1,128 @@
+import { Card, Typography } from "@material-tailwind/react";
+import { deleteUserContactReq, getUserContactRequest } from "../../APIs/userDashboeard";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../Hooks/useAuth";
+import { MdDeleteForever } from "react-icons/md";
+import Swal from "sweetalert2";
 
+const TABLE_HEAD = ["Name",  "Biodata Id", "Status", "Mobile No" ,"Email" , "Dlete Request"];
 const ContactRequests = () => {
+    const { user } = useAuth();
+
+     const { data: TABLE_ROWS = [] , refetch } = useQuery({
+       queryKey: ["success story"],
+       queryFn: () => getUserContactRequest(user.email),
+     });
+    
+     const handleDelete = async (id) => {
+       await deleteUserContactReq(id);
+
+       await refetch();
+       Swal.fire({
+         icon: "error",
+         title: "Done...",
+         text: "Deleted Successfully!",
+       });
+     };
     return (
-        <div>
-            Rerquest
-        </div>
+      <div>
+        <Card className="h-full w-full overflow-x-scroll">
+          <table className="w-full min-w-max table-auto text-left  ">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 "
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-bold leading-none opacity-70 text-lg"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {TABLE_ROWS.map(
+                ({
+                  name,
+                  biodataId,
+                  _id,
+                  status,
+                  mobile_number,
+                  contact_email,
+                }) => (
+                  <tr key={_id} className="even:bg-blue-gray-50/50">
+                    <td className="p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal text-md"
+                      >
+                        {name}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {biodataId}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className={`font-bold ${
+                          status === "approved"
+                            ? "text-green-500"
+                            : "text-red-500"
+                        } `}
+                      >
+                        {status}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {mobile_number}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {contact_email}
+                      </Typography>
+                    </td>
+
+                    <td className="p-4">
+                      <button
+                          onClick={() => handleDelete(_id)}
+                        color="blue-gray"
+                        className="font-medium"
+                      >
+                        <MdDeleteForever className="w-7 h-7"></MdDeleteForever>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </Card>
+      </div>
     );
 };
 
